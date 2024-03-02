f691d333878adc6b98e91c9452fec9565d9e336f
Eduard Aleksandrov <eduard.ale@list.ru>
2024-03-02 19:13:25 +0300

    json added

--- b0201/02_greater/ClassMsgOne.cs
+++ b0201/02_greater/ClassMsgOne.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace St
 {
     public class ClassMsg
@@ -5,6 +7,26 @@ namespace St
         public void Hi() 
         {
             Console.WriteLine("Hello, from ClassMsg!");
+        }
+        public void Convert()
+        {
+            Person tom = new Person("Tom", 38);
+            string json = JsonConvert.SerializeObject(tom);
+            Person? result = JsonConvert.DeserializeObject<Person>(json);
+            Console.WriteLine(json);
+            if(result != null) Console.WriteLine(result.Name);
+            
+        }
+    }
+
+    public class Person
+    {
+        public string Name {get;set;}
+        public int Age {get;set;}
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
         }
     }
 }
--- b0201/02_greater/Program.cs
+++ b0201/02_greater/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
     
-
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string? name = Console.ReadLine();
-        Console.WriteLine($"Hello, World! {name}");
-        
-        St.ClassMsg mc = new St.ClassMsg();
-        mc.Hi();
+        for(;;)
+        {
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hello, World! {name}");
+            
+            St.ClassMsg mc = new St.ClassMsg();
+            mc.Hi();
+            mc.Convert();
 
-        double? x = 5;
-        Values(x);
+            double? x = 5;
+            Values(x);
+        }
+        
     }
 
     public static void Values(double? v)
